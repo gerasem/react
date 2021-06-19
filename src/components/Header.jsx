f970cd05c0316a03ebdbd1249cbbfd223096d280
gerasem <sougift@gmail.com>
2021-06-19 19:01:45 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://via.placeholder.com/300.png/09f/fff%20C/O%20https://placeholder.com/" />
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="image-full-width">
+                <img src="https://via.placeholder.com/900x300?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" />
+            </div>
+            <div className="avatar">
+                <img src="https://cdn.svgporn.com/logos/react.svg" />
+            </div>
+        </div>
+    )
+}
+export default Profile;
