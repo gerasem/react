8f322c8c90fbfed216874f743bcd0caea24acd63
gerasem <sougift@gmail.com>
2021-06-20 23:32:56 +0200

    24: Business Logic Layer start

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,6 +18,20 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Adrian'},
+        {id: 2, name: 'Ann'},
+        {id: 3, name: 'Oleg'},
+        {id: 4, name: 'Administrator'},
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hello, I\'m Dmitry'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'This is not a clickable element, but it\'s an area of the pointer.'},
+    ]
+
     return (
         <div className="columns">
             <div className={style.dialogs}>
@@ -25,10 +39,9 @@ const Dialogs = () => {
                     <p className="title">Dialogs</p>
                     <div className="lists">
                         <ul className={`nes-list is-disc ${style.dialog}`}>
-                            <DialogItem id="1" name="Adrian"/>
-                            <DialogItem id="2" name="Ann"/>
-                            <DialogItem id="3" name="Oleg"/>
-                            <DialogItem id="4" name="Administrator"/>
+
+                            <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
+
                         </ul>
                     </div>
                 </div>
@@ -37,9 +50,9 @@ const Dialogs = () => {
             <div className="column-main column-main-offset-left">
                 <div className="nes-container with-title">
                     <p className="title">Message</p>
-                    <Message message="Hello, I'm Dmitry" />
-                    <Message message="How are you?" />
-                    <Message message="This is not a clickable element, but it's an area of the pointer." />
+
+                    <Message message={messagesData[0].message}/>
+
                 </div>
 
             </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,13 @@ import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id: 1, name: 'Hello World', likeCount: 0},
+        {id: 2, name: 'Hi! How are you?', likeCount: 5},
+        {id: 3, name: 'Its my first post', likeCount: 21},
+    ]
+
     return (
         <section>
             <div className="columns">
@@ -17,9 +24,9 @@ const MyPosts = () => {
             <section className="nes-container with-title">
                 <p className="title">Comments</p>
                 <section className="message-list">
-                    <Post message='Hello World' like='0'/>
-                    <Post message='Hi! How are you?' like='5'/>
-                    <Post message='Its my first post' like='20'/>
+
+                    <Post message={postsData[0].name} like={postsData[0].likeCount}/>
+
                 </section>
             </section>
         </section>
