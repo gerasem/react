94b91abf65f924ce1097403f670d91da359eb5e5
gerasem <sougift@gmail.com>
2021-06-20 21:47:55 +0200

    21: Dialogs css+html

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,45 @@ import style from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className="columns">
+            <div className={style.dialogs}>
+                <div className="nes-container is-dark with-title">
+                    <p className="title">Dialogs</p>
+                    <div className="lists">
+                        <ul className={`nes-list is-disc ${style.dialog}`}>
+                            <li>
+                                <a className={style.active} href="">Adrian</a>
+                            </li>
+                            <li>
+                                <a href="">Ann</a>
+                            </li>
+                            <li>
+                                <a href="">Oleg</a>
+                            </li>
+                            <li>
+                                <a href="">Administrator</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="column-main column-main-offset-left">
+                <div className="nes-container with-title">
+                    <p className="title">Message</p>
+                    <p className="nes-balloon from-left nes-pointer">
+                        This is not a clickable element, but it's an area of the pointer.
+                    </p>
+
+                    <p className="nes-balloon from-left nes-pointer">
+                        This is not a clickable element, but it's an area of the pointer.
+                    </p>
+
+                    <p className="nes-balloon from-left nes-pointer">
+                        This is not a clickable element, but it's an area of the pointer.
+                    </p>
+                </div>
+
+            </div>
         </div>
     )
 }
