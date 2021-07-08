a089a98c03dd79de94a2c5f921cba7bf3f4c28ca
gerasem <sougift@gmail.com>
2021-07-08 22:14:43 +0200

    56: Users: Presentational Components, Container components.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,79 +1,59 @@
-import React from 'react'
-import * as axios from "axios";
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize) // 4.5 -> 5
 
-class Users extends React.Component {
+    let pages = []
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            })
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            })
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize) // 4.5 -> 5
-
-        let pages = []
-
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        return (
-            <div>
-                <h1>Users</h1>
-                {
-                    this.props.users.map(u => <section key={u.id}>
-                            <div className="nes-container">
-                                <div className="columns">
-                                    <div className="column">
-                                        <img className="nes-avatar is-rounded is-large avatar"
-                                             src={u.photos.small != null ? u.photos.small : process.env.PUBLIC_URL + '/avatar.jpg'}/>
-                                    </div>
-                                    <div className="column-main column-main-offset-left">
-                                        <h3>{u.name}</h3>
-                                        <p>{"u.location.city"}, {"u.location.country"}</p>
-                                        <span className="nes-text is-disabled">{u.status}</span>
-                                    </div>
-                                    <div className="column">
-                                        {u.followed
-                                            ? <button onClick={() => {
-                                                this.props.unfollow(u.id)
-                                            }} type="button" className="nes-btn">Unfollow</button>
-                                            : <button onClick={() => {
-                                                this.props.follow(u.id)
-                                            }} type="button" className="nes-btn is-primary">Follow</button>
-                                        }
-                                    </div>
+    return (
+        <div>
+            <h1>Users</h1>
+            {
+                props.users.map(u => <section key={u.id}>
+                        <div className="nes-container">
+                            <div className="columns">
+                                <div className="column">
+                                    <img className="nes-avatar is-rounded is-large avatar"
+                                         src={u.photos.small != null ? u.photos.small : process.env.PUBLIC_URL + '/avatar.jpg'}/>
                                 </div>
-
+                                <div className="column-main column-main-offset-left">
+                                    <h3>{u.name}</h3>
+                                    <p>{"u.location.city"}, {"u.location.country"}</p>
+                                    <span className="nes-text is-disabled">{u.status}</span>
+                                </div>
+                                <div className="column">
+                                    {u.followed
+                                        ? <button onClick={() => {
+                                            props.unfollow(u.id)
+                                        }} type="button" className="nes-btn">Unfollow</button>
+                                        : <button onClick={() => {
+                                            props.follow(u.id)
+                                        }} type="button" className="nes-btn is-primary">Follow</button>
+                                    }
+                                </div>
                             </div>
-                        </section>
-                    )
-                }
 
-                <div>
-                    {pages.map(p => {
-                        return (
-                            <button type="button"
-                                    onClick={(e) => {this.onPageChanged(p)}}
-                                    className={`nes-btn ${this.props.currentPage === p ? 'is-warning' : 'is-primary'}`}>{p}</button>
-                        )
-                    })}
-                </div>
+                        </div>
+                    </section>
+                )
+            }
+
+            <div>
+                {pages.map(p => {
+                    return (
+                        <button type="button"
+                                onClick={(e) => {
+                                    props.onPageChanged(p)
+                                }}
+                                className={`nes-btn ${props.currentPage === p ? 'is-warning' : 'is-primary'}`}>{p}</button>
+                    )
+                })}
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default Users
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,39 @@
 import {connect} from "react-redux";
-import Users from "./Users";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import React from "react";
+import * as axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            users={this.props.users}
+            onPageChanged={this.onPageChanged}
+            currentPage={this.props.currentPage}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -32,6 +65,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
-
-export default UsersContainer
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
